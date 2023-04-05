a6ac8327d38b8672d24dfc5f38d67d588c6044e8
minji9988 <rnwjf111@daum.net>
2023-04-05 23:32:00 +0900

    register

--- app/src/main/java/com/example/clubmanagement/SplashActivity.java
+++ app/src/main/java/com/example/clubmanagement/SplashActivity.java
@@ -18,12 +18,11 @@ class SplashActivity extends AppCompatActivity {
 
         //splash 3초 동안 뜨게 한다.
         Handler hd = new Handler();
-        hd.postDelayed(new splashHandler(), 3000);
+        hd.postDelayed(new splashHandler(), 3000); //3000은 3초란 뜻이다. 1000은 1초다.
+        //postDelayed 메서드를 통해 3초 뒤에 SplashHandler 작동하도록 설정
     }
 
-    //SplashHandler 클래스를 만들고 그 안에 실행하고자 하는 스레드를 작성 후
-//onCreate에 핸들러 선언과 postDelayed 매소드를 이용하여 3초후에 SplashHandler가 작동하도록 해줍니다.
-//      (ex 1초 : 1000)
+    //SplashHandler 클래스 생성
     private class splashHandler implements Runnable {
         public  void run() {
             startActivity(new Intent(getApplication(), MainActivity.class));
