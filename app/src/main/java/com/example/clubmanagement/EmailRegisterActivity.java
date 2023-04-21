ac9f8414a3c926c7d6dc4edbb83c7774092e990f
minji9988 <rnwjf111@daum.net>
2023-04-21 10:42:31 +0900

    register

--- app/src/main/java/com/example/clubmanagement/EmailRegisterActivity.java
+++ app/src/main/java/com/example/clubmanagement/EmailRegisterActivity.java
@@ -5,12 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public
-class activity_register extends AppCompatActivity {
+class EmailRegisterActivity extends AppCompatActivity {
 
     @Override
     protected
     void onCreate ( Bundle savedInstanceState ) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_email_register);
     }
 }
--- app/src/main/java/com/example/clubmanagement/MainActivity.java
+++ app/src/main/java/com/example/clubmanagement/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
 
 public
 class MainActivity extends AppCompatActivity {
@@ -15,6 +17,17 @@ class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        // 로그인 페이지에 있는 '회원가입' 텍스트 클릭 시
+        // EmailRegisterActivity(회원가입 페이지)로 이동
+        TextView txtRegister = (TextView) findViewById(R.id.registerTextButton);
+        txtRegister.setOnClickListener(new View.OnClickListener() {
+            public void onClick( View view) {
+                Intent intent = new Intent(MainActivity.this , EmailRegisterActivity.class);
+                startActivity(intent);
+
+            }
+        });
 
     }
 
